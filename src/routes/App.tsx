d0d72a9943b22c9394b5be94cbfff5ea1d7711e4
Thiago Goulart <thiago.goulart001@edu.pucrs.br>
2025-07-31 11:39:44 -0300

    rotas

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,31 +1,31 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { HomePage as Home } from "../pages/home/home-page";
+import { NavbarProvider } from "../components/navbar/navbar-provider";
+import { AuthProvider } from "../context/auth-context";
+import { ShipmentsProvider } from "../context/shipments-context";
 import { EnviosPage } from "../pages/envios/envios-page";
+import { HomePage as Home } from "../pages/home/home-page";
 import { LoginPage as Login } from "../pages/login/login-page";
+import NovoEnvioPage from "../pages/novo-envio/novo-envio";
 import { RegisterPage as Register } from "../pages/register/register-page";
 import { Settings } from "../pages/settings/Settings";
-import NovoEnvioPage from "../pages/novo-envio/novo-envio";
-import { ShipmentsProvider } from "../context/shipments-context";
-import { AuthProvider } from "../context/auth-context";
-import { NavbarProvider } from "../components/navbar/navbar-provider";
 
 export const App = () => {
-    return (
-        <AuthProvider>
-            <ShipmentsProvider>
-                <NavbarProvider>
-                    <Router>
-                        <Routes>
-                            <Route path="/" element={<Login />} />
-                            <Route path="/register" element={<Register />} />
-                            <Route path="/home" element={<Home />} />
-                            <Route path="/envios" element={<EnviosPage />} />
-                            <Route path="/novo-envio" element={<NovoEnvioPage />} />
-                            <Route path="/settings" element={<Settings />} />
-                        </Routes>
-                    </Router>
-                </NavbarProvider>
-            </ShipmentsProvider>
-        </AuthProvider>
-    );
-}; 
+  return (
+    <AuthProvider>
+      <ShipmentsProvider>
+        <NavbarProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/rregister" element={<Register />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/envios" element={<EnviosPage />} />
+              <Route path="/novo-envio" element={<NovoEnvioPage />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </Router>
+        </NavbarProvider>
+      </ShipmentsProvider>
+    </AuthProvider>
+  );
+};
